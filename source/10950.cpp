ce8290f5de38d2f748347e219e53ad889c1a5429
yeGenieee <cyj95428@gmail.com>
2018-10-14 23:52:35 +0900

    Add 1000.cpp 10950.cpp 2557.cpp 2558.cpp

---
+++ source/10950.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    int a,b;
+
+    cin>>t;
+    
+    while(t--) {
+        cin>> a >> b;
+        cout<< a+b << "\n";
+    }
+
+    return 0;
+}
