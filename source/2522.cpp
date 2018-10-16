5d35ca52430a85e75909f95dd9e9aa488847f8a5
yeGenieee <cyj95428@gmail.com>
2018-10-17 00:43:04 +0900

    Add 2522.cpp

---
+++ source/2522.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin>>n;
+
+    for(int i=0;i<n;i++) {
+        for(int k=0;k<n-i-1;k++) {
+            cout<<" ";
+        }
+        for(int j=0;j<i+1;j++) {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    for(int i=0;i<n-1;i++) {
+        for(int k=0;k<i+1;k++) {
+            cout<<" ";
+        }
+        for(int j=0;j<n-i-1;j++) {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
