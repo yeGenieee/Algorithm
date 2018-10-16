124cf89f30add65fbc0098b9722f01f54604ef45
yeGenieee <cyj95428@gmail.com>
2018-10-16 21:52:57 +0900

    Add 2445.cpp

--- source/2445.cpp
+++ source/2445.cpp
@@ -4,33 +4,36 @@ using namespace std;
 
 int main() {
     int n;
-
+    
     cin>>n;
-
+    
     for(int i=1; i<=n; i++) {
-        for (int k=0;k<(n-i);k++) {
+        for(int j=0;j<i;j++) {
             cout<<"*";
         }
-        for(int j=0;j<(2*i-1); j++) {
+        for(int k=0;k<(2*(n-i)); k++) {
             cout<<" ";
         }
-        for (int k=0;k<(n-i);k++) {
+        
+        for(int j=0;j<i;j++) {
             cout<<"*";
         }
         
         cout<<"\n";
     }
     for(int i=n-1; i>0; i--) {
-        for (int k=0;k<(n-i);k++) {
-            cout<<" ";
-        }
-        for(int j=0;j<(2*i-1); j++) {
+        for(int j=0;j<i;j++) {
             cout<<"*";
         }
-        cout<<" ";
-
+        for(int k=0;k<(2*(n-i)); k++) {
+            cout<<" ";
+        }
+        for(int j=0;j<i;j++) {
+            cout<<"*";
+        }
+        
         cout<<"\n";
     }
-
+    
     return 0;
 }
