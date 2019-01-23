fc0bc45bc7a09b3501ca15b11e18d7ce8521ae4a
yeGenieee <cyj95428@gmail.com>
2019-01-23 22:13:38 +0900

    Leetcode_FindPivotIndex Problem_Artificial Sol

--- source/leet-code/src/main/java/array/FindPivotIndex.java
+++ source/leet-code/src/main/java/array/FindPivotIndex.java
@@ -21,4 +21,26 @@ public class FindPivotIndex {
 
         return pivot;
     }
+
+    /**
+     * nums 배열 원소의 전체 합 : sum, 인덱스 i를 기준으로 그 왼쪽 원소들의 합 : leftsum
+     * 인덱스 i를 기준으로 그 오른쪽 원소들의 합 : S - leftsum - nums[i]
+     * leftsum과 S - leftsum - nums[i] 가 같아지는 인덱스 i가 pivot Index
+     *
+     * Time Complexity : O(N) nums 배열의 길이만큼 for 문
+     * Space Complexity : O(1) leftsum, S 메모리 사용
+     * */
+    public int pivotIndexOfficialSolution(int[] nums) {
+        int sum=0, leftsum=0;
+
+        for(int x:nums) sum += x;
+        for(int i=0;i<nums.length;i++) {
+            if (leftsum == (sum - leftsum - nums[i])) {
+                return i;
+            }
+            leftsum += nums[i];
+        }
+
+        return -1;
+    }
 }
