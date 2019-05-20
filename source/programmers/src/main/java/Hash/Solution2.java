88b5db91662c11c08ff34d97b18acc9c8aa74d36
yeGenieee <cyj95428@gmail.com>
2019-05-21 00:49:28 +0900

    [해시] 전화번호부 - startsWith로 풀이 더 간결하게

--- source/programmers/src/main/java/Hash/Solution2.java
+++ source/programmers/src/main/java/Hash/Solution2.java
@@ -4,14 +4,13 @@ public class Solution2 {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
 
-        for (int i=0;i<phone_book.length;i++) {
-            for(int j=0;j<phone_book.length;j++) {
-                if (phone_book[j].startsWith(phone_book[i]) && i!=j) {
-                    return false;
-                }
+        for (int i=0;i<phone_book.length-1;i++) {
+            for(int j=i+1;j<phone_book.length;j++) {
+                if (phone_book[j].startsWith(phone_book[i])) return false;
+                if (phone_book[i].startsWith(phone_book[j])) return false;
             }
         }
 
-        return answer;
+        return true;
     }
 }
