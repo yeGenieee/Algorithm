c3141f450031fd786482b9ff1c65a7be3920d1af
yeGenieee <cyj95428@gmail.com>
2019-05-21 00:46:04 +0900

    [해시] 전화번호부 - startsWith로 풀이

--- source/programmers/src/main/java/Hash/Solution2.java
+++ source/programmers/src/main/java/Hash/Solution2.java
@@ -1,4 +1,17 @@
 package Hash;
 
 public class Solution2 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        for (int i=0;i<phone_book.length;i++) {
+            for(int j=0;j<phone_book.length;j++) {
+                if (phone_book[j].startsWith(phone_book[i]) && i!=j) {
+                    return false;
+                }
+            }
+        }
+
+        return answer;
+    }
 }
