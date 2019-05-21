a8951ec74e596f36886d6c636ff5a56aa1b93c65
yeGenieee <cyj95428@gmail.com>
2019-05-22 00:56:43 +0900

    [해시] 위장 완료

---
+++ source/programmers/src/main/java/Hash/Solution3.java
@@ -0,0 +1,39 @@
+package Hash;
+
+import java.util.HashMap;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        String[][] input1 = {
+                            {"yellow_hat","headgear"},
+                            {"blue_sunglasses", "eyewear"},
+                            {"green_turban", "headgear"}
+                            };
+        String[][] input2 = {
+                {"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+
+        System.out.println(solution(input1));
+        System.out.println(solution(input2));
+
+    }
+
+    public static int solution(String[][] clothes) {
+        int answer = 0;
+
+        HashMap<String,String> hm = new HashMap<>();
+
+        for (int i=0;i<clothes.length;i++)
+            hm.put(clothes[i][0],clothes[i][1]);
+
+        answer += clothes.length;
+
+        for (int i=0;i<clothes.length-1;i++) {
+            for(int j=i+1;j<clothes.length;j++) {
+                if (hm.get(clothes[i][0]) != hm.get(clothes[j][0]))
+                    answer++;
+            }
+        }
+
+        return answer;
+    }
+}
