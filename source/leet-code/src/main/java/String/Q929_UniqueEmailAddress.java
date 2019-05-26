19d9d45ab94461a574b2d3e14d2a568842d514d7
yeGenieee <cyj95428@gmail.com>
2019-05-26 22:00:48 +0900

    [String] Q929. Unique Eamil Address

--- source/leet-code/src/main/java/String/Q929_UniqueEmailAddress.java
+++ source/leet-code/src/main/java/String/Q929_UniqueEmailAddress.java
@@ -1,4 +1,27 @@
 package String;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Q929_UniqueEmailAddress {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> receiver = new HashSet();
+
+        for (String email : emails) {
+            int index = email.indexOf('@');
+
+            String local = email.substring(0,index);
+            String domain = email.substring(index);
+
+            if (local.contains("+")) {
+                local = local.substring(0,local.indexOf('+'));
+            }
+
+            local = local.replaceAll("\\." , "");
+
+            receiver.add(local+domain);
+        }
+
+        return receiver.size();
+    }
 }
