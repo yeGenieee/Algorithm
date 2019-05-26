00931bc232474ddd50bc87882158dd098ee463fe
yeGenieee <cyj95428@gmail.com>
2019-05-26 22:02:02 +0900

    [String] Q804. Unique Morse Code Words

--- source/leet-code/src/main/java/String/Q804_UniqueMorseCodeWords.java
+++ source/leet-code/src/main/java/String/Q804_UniqueMorseCodeWords.java
@@ -1,4 +1,38 @@
 package String;
 
+import java.util.HashMap;
+
 public class Q804_UniqueMorseCodeWords {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morseCode = {
+                                ".-","-...","-.-.","-..",".",
+                                "..-.","--.","....","..",".---",
+                                "-.-",".-..","--","-.","---",
+                                ".--.","--.-",".-.","...","-",
+                                "..-","...-",".--","-..-","-.--","--.."
+                            };
+
+        String[] transformation = new String[words.length];
+
+        HashMap<String,Integer> hm = new HashMap<String,Integer>();
+
+        for (int i=0;i<words.length;i++) {
+            String str = "";
+            for(int j=0;j<words[i].length();j++) {
+                str += morseCode[words[i].charAt(j)-97];
+            }
+            transformation[i] = str;
+        }
+
+        for (int i=0;i<transformation.length;i++) {
+            if (hm.get(transformation[i]) == null) {
+                hm.put(transformation[i], 1);
+            } else {
+                hm.put(transformation[i], hm.get(transformation[i])+1);
+            }
+        }
+
+        return hm.size();
+
+    }
 }
