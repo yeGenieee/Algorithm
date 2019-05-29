c08556c1988e954de3672e09663f5826f5c0261e
yeGenieee <cyj95428@gmail.com>
2019-05-29 12:49:22 +0900

    [String] Q557 Reverse Words In a String III

---
+++ source/leet-code/src/main/java/String/Q557_ReverseWordsInAString3.java
@@ -0,0 +1,21 @@
+package String;
+
+public class Q557_ReverseWordsInAString3 {
+    public String reverseWords(String s) {
+        String answer = "";
+
+        String[] splitArray = s.split(" ");
+
+        for(int i=0; i<splitArray.length; i++) {
+            StringBuilder sb = new StringBuilder(splitArray[i]);
+            sb = sb.reverse();
+            if (i != splitArray.length-1) {
+                answer = answer + sb.toString() + " ";
+            } else {
+                answer = answer + sb.toString();
+            }
+        }
+
+        return answer;
+    }
+}
