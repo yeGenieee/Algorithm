8834a0f0c31f751e1f394c3769f0b898e9e77fc4
yeGenieee <cyj95428@gmail.com>
2019-09-22 16:22:52 +0900

    [ADD] Solved BOJ Q10845_큐

--- source/BOJ/src/main/java/DataStructure1/Queue/Q10845.java
+++ source/BOJ/src/main/java/DataStructure1/Queue/Q10845.java
@@ -6,53 +6,95 @@ import java.util.Queue;
 
 public class Q10845 {
     public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
-        int N = stdIn.nextInt();
-        stdIn.nextLine();
+//        Scanner stdIn = new Scanner(System.in);
+//        int N = stdIn.nextInt();
+//        stdIn.nextLine();
+//
+//        Queue<Integer> queue = new LinkedList<Integer>();
+//
+//        for (int i=0;i<N;i++) {
+//            String cmdLine = stdIn.nextLine();
+//            String cmd = cmdLine.split(" ")[0];
+//
+//            if (cmd.equals("push")) {
+//                queue.add(Integer.parseInt(cmdLine.split(" ")[1]));
+//            } else if (cmd.equals("pop")) {
+//                if (queue.isEmpty()) {
+//                    System.out.println(-1);
+//                } else {
+//                    System.out.println(queue.poll());
+//                }
+//            } else if (cmd.equals("size")) {
+//                if (queue.isEmpty()) {
+//                    System.out.println(0);
+//                } else {
+//                    System.out.println(queue.size());
+//                }
+//            } else if (cmd.equals("empty")) {
+//                if (queue.isEmpty()) {
+//                    System.out.println(1);
+//                } else {
+//                    System.out.println(0);
+//                }
+//            } else if (cmd.equals("front")) {
+//                if (queue.isEmpty()) {
+//                    System.out.println(-1);
+//                } else {
+//                    System.out.println(queue.peek());
+//                }
+//            } else if (cmd.equals("back")) {
+//                if (queue.isEmpty()) {
+//                    System.out.println(-1);
+//                } else {
+//                    System.out.println(((LinkedList<Integer>) queue).peekLast());
+//                }
+//
+//            } else {
+//
+//            }
+//        }
+        /**
+         *  배열을 이용한 큐의 구현
+         * */
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
 
-        Queue<Integer> queue = new LinkedList<Integer>();
+        int[] queue = new int[num];
+        int begin = 0;
+        int end = 0;
 
-        for (int i=0;i<N;i++) {
-            String cmdLine = stdIn.nextLine();
-            String cmd = cmdLine.split(" ")[0];
-
+        while (num-- > 0) {
+            String cmd = sc.next();
             if (cmd.equals("push")) {
-                queue.add(Integer.parseInt(cmdLine.split(" ")[1]));
+                int x = Integer.parseInt(sc.next());
+                queue[end++] = x;
             } else if (cmd.equals("pop")) {
-                if (queue.isEmpty()) {
-                    System.out.println(-1);
+                if (begin == end) {
+                    System.out.println("-1");
                 } else {
-                    System.out.println(queue.poll());
+                    System.out.println(queue[begin++]);
                 }
             } else if (cmd.equals("size")) {
-                if (queue.isEmpty()) {
-                    System.out.println(0);
-                } else {
-                    System.out.println(queue.size());
-                }
+                System.out.println(end-begin);
             } else if (cmd.equals("empty")) {
-                if (queue.isEmpty()) {
-                    System.out.println(1);
+                if (begin == end) {
+                    System.out.println("1");
                 } else {
-                    System.out.println(0);
+                    System.out.println("0");
                 }
             } else if (cmd.equals("front")) {
-                if (queue.isEmpty()) {
-                    System.out.println(-1);
+                if (begin == end) {
+                    System.out.println("-1");
                 } else {
-                    System.out.println(queue.peek());
+                    System.out.println(queue[begin]);
                 }
             } else if (cmd.equals("back")) {
-                if (queue.isEmpty()) {
-                    System.out.println(-1);
+                if (begin == end) {
+                    System.out.println("-1");
                 } else {
-                    System.out.println(((LinkedList<Integer>) queue).peekLast());
+                    System.out.println(queue[end-1]);
                 }
-
-            } else {
-
             }
         }
-
     }
 }
