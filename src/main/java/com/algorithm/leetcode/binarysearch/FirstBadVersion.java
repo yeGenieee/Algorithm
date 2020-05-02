6e425d1bf25b9174fdfa89c21c3a1c3bb95e6d1b
yeGenieee <cyj95428@gmail.com>
2020-05-02 16:00:44 +0900

    Add Leetcode - Q278_FirstBadVersion

--- src/main/java/com/algorithm/leetcode/binarysearch/FirstBadVersion.java
+++ src/main/java/com/algorithm/leetcode/binarysearch/FirstBadVersion.java
@@ -1,12 +1,17 @@
 package com.algorithm.leetcode.binarysearch;
 
 public class FirstBadVersion {
+    /**
+     * for 문을 이용한 solution --> Time Limit Exceeded
+     * Time Complexity : O(log n)
+     * Space Somplexity : O(1)
+     */
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
 
-        while(left < right) {
-            int mid = left + (right - left) / 2;
+        while(left < right) { // 이진 탐색 종료 조건 : left 가 right를 넘어서는 경우 -> 그 때의 left가 firstBadVersion
+            int mid = left + (right - left) / 2; // overflow 방지
             if (isBadVersion(mid)) {
                 right = mid;
             } else {
