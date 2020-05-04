1c5ff0c0f165b1d1c34ca00b1429e47f70848bbc
yeGenieee <cyj95428@gmail.com>
2020-05-04 23:14:26 +0900

    Add Programmers - StringToInteger (문자열을 정수로 바꾸기)

---
+++ src/main/java/com/algorithm/programmers/string/StringToInteger.java
@@ -0,0 +1,15 @@
+package com.algorithm.programmers.string;
+
+public class StringToInteger {
+    /**
+     * TimeComplexity : O(1)
+     * SpaceComplexity : O(1)
+     * */
+    public int solution(String s) {
+        int answer = 0;
+
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
