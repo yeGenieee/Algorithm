febeea81b34350293e7ccf38967e2a031b22aff3
yeGenieee <cyj95428@gmail.com>
2021-01-15 23:03:09 +0900

    Add Programmers: 제일 작은 수 제거하기

---
+++ src/main/java/com/algorithm/programmers/basic/removeMinimum.java
@@ -0,0 +1,28 @@
+package com.algorithm.programmers.basic;
+
+public class removeMinimum {
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) {
+            return new int[] {-1};
+        }
+        int[] answer = new int[arr.length - 1];
+
+        int minIndex = 0;
+        for (int i=0; i<arr.length; i++) {
+            if (arr[minIndex] > arr[i]) {
+                minIndex = i;
+            }
+        }
+
+        for (int i=minIndex+1; i<arr.length; i++) {
+            arr[i-1] = arr[i];
+        }
+
+        for (int i=0; i<answer.length; i++) {
+            answer[i] = arr[i];
+        }
+
+        return answer;
+
+    }
+}
