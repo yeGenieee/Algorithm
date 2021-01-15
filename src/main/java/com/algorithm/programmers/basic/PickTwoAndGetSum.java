29604aa3d5b590fa0147265d8670e451d332c776
yeGenieee <cyj95428@gmail.com>
2021-01-15 23:08:28 +0900

    Add Programmers: 두 개 뽑아서 더하기

---
+++ src/main/java/com/algorithm/programmers/basic/PickTwoAndGetSum.java
@@ -0,0 +1,29 @@
+package com.algorithm.programmers.basic;
+
+import java.util.*;
+
+public class PickTwoAndGetSum {
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i=0; i<numbers.length; i++) {
+            for (int j=i; j< numbers.length; j++) {
+                int sum = numbers[i] + numbers[j];
+                if (!list.contains(sum)) {
+                    list.add(sum);
+                }
+            }
+        }
+
+        int[] answer = new int[list.size()];
+
+        for (int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        Arrays.sort(answer);
+
+        return answer;
+
+    }
+}
