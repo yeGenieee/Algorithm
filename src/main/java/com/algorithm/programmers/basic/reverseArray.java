5ae5c71edd3709d6ef962f03d03c0fe4fd4449ab
yeGenieee <cyj95428@gmail.com>
2021-01-15 22:57:43 +0900

    Add Programmers: 자연수 뒤집어 배열로 만들기

---
+++ src/main/java/com/algorithm/programmers/basic/reverseArray.java
@@ -0,0 +1,14 @@
+package com.algorithm.programmers.basic;
+
+public class reverseArray {
+    public int[] solution(long n) {
+        String str = n + "";
+        int[] answer = new int[str.length()];
+
+        for (int i=str.length()-1; i>=0; i--) {
+            answer[i] = (int) str.charAt(str.length() - i - 1) - '0';
+        }
+
+        return answer;
+    }
+}
