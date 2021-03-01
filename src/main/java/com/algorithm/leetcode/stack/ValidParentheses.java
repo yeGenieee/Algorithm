8e7b97d57d445d5f12942d738b8ea09e7ac746c5
yeGenieee <cyj95428@gmail.com>
2021-03-01 16:14:23 +0900

    Add leetcode_ValidParentheses

---
+++ src/main/java/com/algorithm/leetcode/stack/ValidParentheses.java
@@ -0,0 +1,27 @@
+package com.algorithm.leetcode.stack;
+
+import java.util.*;
+
+public class ValidParentheses {
+    public static Map<Character, Character> map = new HashMap<>();
+
+    static {
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (stack.isEmpty() || stack.pop() != map.get(ch)) {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
