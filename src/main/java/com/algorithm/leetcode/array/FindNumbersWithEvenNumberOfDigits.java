e8a2f99b39b6c26c74d6eeee530cf08e1b56a4ae
yeGenieee <cyj95428@gmail.com>
2021-03-04 23:34:34 +0900

    Modify leetcode_Find Numbers with even number of digits

--- src/main/java/com/algorithm/leetcode/array/FindNumbersWithEvenNumberOfDigits.java
+++ src/main/java/com/algorithm/leetcode/array/FindNumbersWithEvenNumberOfDigits.java
@@ -4,17 +4,23 @@ public class FindNumbersWithEvenNumberOfDigits {
     public int findNumbers(int[] nums) {
         int count = 0;
 
-        for (int num : nums) {
-            int digit = 0;
-            while (num > 0) {
-                digit++;
-                num /= 10;
-            }
-            if (digit % 2 == 0) {
+        for (int num : nums ){
+            if (countDigit(num) % 2 == 0) {
                 count++;
             }
         }
 
         return count;
+
+    }
+
+    public int countDigit(int num) {
+        int digit = 0;
+
+        while (num != 0) {
+            digit++;
+            num /= 10;
+        }
+        return digit;
     }
 }
