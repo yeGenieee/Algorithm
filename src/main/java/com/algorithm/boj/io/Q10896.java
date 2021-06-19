9f4d195fb490a65cb93373bbf11dbae85d455d13
yeGenieee <cyj95428@gmail.com>
2021-06-19 10:17:08 +0900

    Add BOJ_Q10896

---
+++ src/main/java/com/algorithm/boj/io/Q10896.java
@@ -0,0 +1,17 @@
+package com.algorithm.boj.io;
+
+import java.util.Scanner;
+
+public class Q10896 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
