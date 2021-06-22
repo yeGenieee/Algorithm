b994d47d637dacfcd2afe239c4298389f57aee62
yeGenieee <cyj95428@gmail.com>
2021-06-22 22:33:16 +0900

    Add BOJ_Q1330

---
+++ src/main/java/com/algorithm/boj/basic/Q1330.java
@@ -0,0 +1,20 @@
+package com.algorithm.boj.basic;
+
+import java.util.Scanner;
+
+public class Q1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
