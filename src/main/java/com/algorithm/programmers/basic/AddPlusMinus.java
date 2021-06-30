b839f590378cef4bcd3ce5138f0effff0167e554
yeGenieee <cyj95428@gmail.com>
2021-06-30 16:38:09 +0900

    Add Programmers_음양더하기

---
+++ src/main/java/com/algorithm/programmers/basic/AddPlusMinus.java
@@ -0,0 +1,13 @@
+package com.algorithm.programmers.basic;
+
+public class AddPlusMinus {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i=0; i<absolutes.length; i++) {
+            answer += absolutes[i] * (signs[i] ? 1 : -1);
+        }
+
+        return answer;
+    }
+}
