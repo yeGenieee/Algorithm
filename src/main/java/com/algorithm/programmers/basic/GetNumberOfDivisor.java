7dd62b0d1fbe1dae85b404c60f60dfb7d1ada161
yeGenieee <cyj95428@gmail.com>
2021-07-05 16:37:28 +0900

    Add Programmers_약수의개수와덧셈

---
+++ src/main/java/com/algorithm/programmers/basic/GetNumberOfDivisor.java
@@ -0,0 +1,31 @@
+package com.algorithm.programmers.basic;
+
+public class GetNumberOfDivisor {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for (int i=left; i <= right; i++) {
+            int cnt = getNumberOfDivisor(i);
+
+            if (cnt % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+
+        return answer;
+    }
+
+    public int getNumberOfDivisor(int num) {
+        int count = 0;
+
+        for (int i=1; i<=num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
