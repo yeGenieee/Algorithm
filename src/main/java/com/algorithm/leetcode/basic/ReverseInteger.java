db7c9ec87439ec5df14ed80b90ecb25d08356d75
yeGenieee <cyj95428@gmail.com>
2021-07-17 12:20:41 +0900

    Add Leetcode_ReverseInteger

---
+++ src/main/java/com/algorithm/leetcode/basic/ReverseInteger.java
@@ -0,0 +1,24 @@
+package com.algorithm.leetcode.basic;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        int answer = 0;
+
+        while (x != 0) {
+            int last = x % 10;
+            x /= 10;
+
+            if (answer > Integer.MAX_VALUE/10 || (answer == Integer.MAX_VALUE / 10 && last > 7)) {
+                return 0;
+            }
+
+            if (answer < Integer.MIN_VALUE/10 || (answer == Integer.MIN_VALUE / 10 && last < -8)) {
+                return 0;
+            }
+
+            answer = answer * 10 + last;
+        }
+
+        return answer;
+    }
+}
