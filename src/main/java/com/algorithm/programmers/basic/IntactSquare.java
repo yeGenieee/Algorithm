ddddfb10e65eacc68e1ac6062196b65b2cc07ca2
yeGenieee <cyj95428@gmail.com>
2021-07-18 11:49:42 +0900

    Add Programmers_멀쩡한사각형

---
+++ src/main/java/com/algorithm/programmers/basic/IntactSquare.java
@@ -0,0 +1,13 @@
+package com.algorithm.programmers.basic;
+
+public class IntactSquare {
+    public long solution(int w, int h) {
+        long answer = 0;
+
+        for (int i=0; i<w; i++) {
+            answer += (Long.valueOf(h) * i) / Long.valueOf(w);
+        }
+
+        return answer * 2;
+    }
+}
