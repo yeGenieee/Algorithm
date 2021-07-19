b5646677f73b84da627829d0b40f38b48c3b05fc
yeGenieee <cyj95428@gmail.com>
2021-07-19 12:59:41 +0900

    Add Leetcode_PalindromeNumber

---
+++ src/main/java/com/algorithm/leetcode/basic/PalindromeNumber.java
@@ -0,0 +1,18 @@
+package com.algorithm.leetcode.basic;
+
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedNum = 0;
+
+        while (x > reversedNum) {
+            reversedNum = reversedNum * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return x == reversedNum || x == reversedNum / 10;
+    }
+}
