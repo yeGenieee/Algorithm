58406c3a72c997bf181457e5f8935dde191d4f8b
yeGenieee <cyj95428@gmail.com>
2021-07-26 23:50:19 +0900

    Add Leetcode_MergeSortedArray

---
+++ src/main/java/com/algorithm/leetcode/array/MergeSortedArray.java
@@ -0,0 +1,14 @@
+package com.algorithm.leetcode.array;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i=0; i<n; i++) {
+            nums1[i+m] = nums2[i];
+        }
+
+        Arrays.sort(nums1);
+
+    }
+}
