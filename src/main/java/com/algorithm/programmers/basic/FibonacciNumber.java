ccdc94a8607dc97019939e08b992695e48c88a51
yeGenieee <cyj95428@gmail.com>
2021-07-30 23:38:44 +0900

    Add Programmers_피보나치수

---
+++ src/main/java/com/algorithm/programmers/basic/FibonacciNumber.java
@@ -0,0 +1,21 @@
+package com.algorithm.programmers.basic;
+
+public class FibonacciNumber {
+    public int solution(int n) {
+        int answer = 0;
+        int num1 = 1;
+        int num2 = 1;
+
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            for (int i = 3; i <= n; i++) {
+                answer = (num1 + num2) % 1234567;
+                num1 = num2;
+                num2 = answer;
+            }
+        }
+
+        return answer;
+    }
+}
