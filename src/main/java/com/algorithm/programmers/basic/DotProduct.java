0a3e76274a6fa27ec53f5bc9195b946bcbfd24ba
yeGenieee <cyj95428@gmail.com>
2021-08-12 23:45:15 +0900

    Add Programmers_내적

---
+++ src/main/java/com/algorithm/programmers/basic/DotProduct.java
@@ -0,0 +1,14 @@
+package com.algorithm.programmers.basic;
+
+public class DotProduct {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i=0; i<a.length; i++) {
+            int current = a[i] * b[i];
+            answer += current;
+        }
+
+        return answer;
+    }
+}
