409f17de8f7bf3107053c0a5d6178fdf29c0ef7e
yeGenieee <cyj95428@gmail.com>
2021-09-08 23:54:03 +0900

    Add BOJ_Q11654_아스키코드

---
+++ src/main/java/com/algorithm/boj/basic/Q11654.java
@@ -0,0 +1,13 @@
+package com.algorithm.boj.basic;
+
+import java.util.Scanner;
+
+public class Q11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int ch = scanner.next().charAt(0);
+
+        System.out.println(ch);
+    }
+}
