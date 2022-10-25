9597d03bc00aa1f558a09ebdfc76b5de46bf6d2e
yeGenieee <cyj95428@gmail.com>
2022-10-26 00:06:23 +0900

    Add TwoSum2

--- src/main/java/com/algorithm/leetcode/array/TwoSum.java
+++ src/main/java/com/algorithm/leetcode/array/TwoSum.java
@@ -1,4 +1,13 @@
+import java.util.*;
+
 public class TwoSum {
+    /**
+     * 시간복잡도 : O(N^2)
+     * 공간복잡도 : O(1)
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -7,6 +16,32 @@ public class TwoSum {
                 }
             }
         }
+        return null;
+    }
+
+    /**
+     * 시간복잡도 : O(N)
+     * 공간복잡도 : O(N)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++ {
+            int current = nums[i];
+            // current + x = target
+            // x = target - current;
+            int x = target - current;
+
+            if (map.containsKey(x)) {
+                return new int[] { map.get(x), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
         return null;
     }
 }
