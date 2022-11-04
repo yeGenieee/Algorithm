27c95fd359ab6faa25cd813feedb96897d69da57
yeGenieee <cyj95428@gmail.com>
2022-11-04 23:07:25 +0900

    Add FrogJump

--- src/main/java/com/algorithm/codility/timecomplexity/FrogJmp.java
+++ src/main/java/com/algorithm/codility/timecomplexity/FrogJmp.java
@@ -1,15 +1,24 @@
 package com.algorithm.codility.timecomplexity;
 
 public class FrogJmp {
-    public int solution(int X, int Y, int D) {
-        int currentPos = X;
-        int cnt = 0;
+//    public int solution(int X, int Y, int D) {
+//        int currentPos = X;
+//        int cnt = 0;
+//
+//        while (X <= Y) {
+//            ++cnt;
+//            X += D;
+//        }
+//
+//        return cnt;
+//    }
 
-        while (X <= Y) {
-            ++cnt;
-            X += D;
+    public int solution(int X, int Y, int D) {
+        int distance = Y - X;
+        if (distance <= 0) {
+            return 0;
         }
 
-        return cnt;
+        return Math.ceil((double) distance / D);
     }
 }
