9ff51af7809a1614d1f852d03cc6d75b2b7c5b87
yeGenieee <cyj95428@gmail.com>
2022-11-13 20:10:38 +0900

    Add Binary2

--- src/main/java/com/algorithm/leetcode/string/AddBinary.java
+++ src/main/java/com/algorithm/leetcode/string/AddBinary.java
@@ -1,10 +1,13 @@
-/**
- * 문자열의 각 자리수끼리 더하고,
- * 더한 값에 대해 올림이 있는 경우
- * 자리 올림을 반영하도록 함
- * 시간 복잡도 : O(Max(N, M)) --> a, b 길이 중 긴 값에 대해 O(N)
- */
+import java.math.BigInteger;
+
 public class AddBinary {
+    /**
+     * 문자열의 각 자리수끼리 더하고,
+     * 더한 값에 대해 올림이 있는 경우
+     * 자리 올림을 반영하도록 함
+     * 시간 복잡도 : O(Max(N, M)) --> a, b 길이 중 긴 값에 대해 O(N)
+     * 공간 복잡도 : O(Max(N,M))
+     */
     public String addBinary(String a, String b) {
         int n = a.length();
         int m = b.length();
@@ -42,5 +45,31 @@ public class AddBinary {
         sb.reverse();
 
         return sb.toString();
+    }
+
+    /**
+     * Bit Manipulation 을 이용
+     * XOR을 이용하여 더한 값을 구하고,
+     * AND 연산자와 << 1 (왼쪽 시프트 연산자) 를 이용하여 carry (올림) 을 구한다
+     * carry가 0 이 아닐 동안 반복
+     * 시간 복잡도 : O(N + M)
+     * 공간 복잡도 : O(Max(N,M))
+     */
+    public String addBinary2(String a, String b) {
+        // Input이 2^100 이상의 값이 될 수도 있으므로 BigInteger를 사용한다
+        BigInteger x = new BigInteger(a, 2);
+        BigInteger y = new BigInteger(b, 2);
+        BigInteger zero = new BigInteger("0", 2);
+        BigInteger answer, carry;
+
+        while (y.compareTo(zero) != 0) {
+            answer = x.xor(y);
+            carry = x.and(y).shiftLeft(1);
+
+            x = answer;
+            y = carry;
+        }
+
+        return x.toString(2); // 이진수 형태의 string으로 반환
     }
 }
